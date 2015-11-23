4670d4c145b279e80dc4b44dbf046052c33d3535
spacebear5000 <david.giahuy.tu@gmail.com>
2015-11-23 13:56:42 -0500

    site mk. II is go

---
+++ js/index.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+  $(function(){
+    $(".element").typed({
+      stringsElement: $('#paragraph'),
+      typeSpeed: 3,
+      showCursor: false,
+    });
+  });
+  // $('body').hover('.element a', function(event){
+  //   console.log($(this).css())
+  // });
+
+
+
+});
